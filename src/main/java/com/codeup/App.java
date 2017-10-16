802705983f5cb0f25d4754b34786fcdc509c9363
chasehammock <chasehammock@gmail.com>
2017-10-16 10:00:32 -0500

    added initial files to the repo

---
+++ src/main/java/com/codeup/App.java
@@ -0,0 +1,20 @@
+package com.codeup;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    boolean isBlank = StringUtils.isBlank("   ");
+
+
+}
